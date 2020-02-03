a81812802e38bf96f1b50f7a033c02b061e50cc0
mmc8629 <60587959+mmc8629@users.noreply.github.com>
2020-02-04 10:31:52 +1300

    Update Hello world tast 1.py

--- Hello world tast 1.py
+++ Hello world tast 1.py
@@ -1,3 +1,15 @@
-"""sasrsza vMichael McCaskey 3/2/2020"""
 
-print("hello bro")
+"""day 2"""
+# variables should have appropriate names
+name = "Michael"
+# if there are multiple words in the variable name, use underscores
+last_name = "McCaskey"
+age = 15
+
+
+print(name, age, last_name)
+# when getting input, use the imput() function 
+# we need to set a variable to store the input
+fav_animal = input("what is your favourite animal?")
+
+print("wrong! a", fav_animal, "is nothing compared to the majestic rain turtle.")
